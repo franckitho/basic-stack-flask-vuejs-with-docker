cd435ea1366fe623677c6eb8d0c336e39aa1279e
Franck Colonna <33814441+noxyz20@users.noreply.github.com>
2021-06-12 19:16:56 +0200

    Update HelloWorld.vue

--- front/app/src/components/HelloWorld.vue
+++ front/app/src/components/HelloWorld.vue
@@ -28,7 +28,7 @@ export default {
 
   // Fetches posts when the component is created.
   created () {
-    axios.get('http://localhost:5000//api/users')
+    axios.get('http://localhost:5000/api/users')
       .then(response => {
         // JSON responses are automatically parsed.
         this.users = response.data
